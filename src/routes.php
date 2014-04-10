f12c9f6e0e87b3a70ec3723933167155be643db6
Björn Martensen <bjoern.martensen@gmail.com>
2014-04-10 17:43:40 +0200

    group main routes to grab www. and no subdomain main app routes

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,12 @@
 <?php
 
-Route::group(array('domain' => 'www.' . Config::get('app.domain'), 'before' => 'handleRequests|handleMainApp'), function () {
-    Route::any('/', function () {
-        return "generic route for main app.";
-    });
+Route::group(array('before' => 'handleRequests|handleMainApp'), function() {
+    $mainAppRoutes = function () {
+        Route::any('/', function () {
+            return "generic route for main app.";
+        });
+    };
+
+    Route::group(array('domain' => 'www.' . Config::get('app.domain')), $mainAppRoutes );
+    Route::group(array('domain' => Config::get('app.domain')), $mainAppRoutes );
 });
