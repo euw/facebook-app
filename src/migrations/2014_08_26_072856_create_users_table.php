397f4b10936e52f54cb294746ed4e1ef062dcb47
Björn Martensen <bjoern.martensen@gmail.com>
2014-08-26 12:51:04 +0200

    fix migrations

--- src/migrations/2014_08_26_072856_create_users_table.php
+++ src/migrations/2014_08_26_072856_create_users_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRequestsTable extends Migration {
+class CreateUsersTable extends Migration {
 
     /**
      * Run the migrations.
@@ -12,10 +12,13 @@ class CreateRequestsTable extends Migration {
      */
     public function up()
     {
-        Schema::create('requests', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->increments('id');
-            $table->string('request_id');
+
+            $table->text('terms_of_use');
+            $table->text('privacy_policy');
+            $table->string('share_message', 255);
 
             $table->integer('tenant_id')->unsigned()->nullable();
             $table->foreign('tenant_id')
@@ -33,7 +36,7 @@ class CreateRequestsTable extends Migration {
      */
     public function down()
     {
-        Schema::drop('requests');
+        Schema::drop('users');
     }
 
 }
