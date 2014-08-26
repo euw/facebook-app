f9f34b8fdcb60e48bdae83cae2765518c0eee6fe
Björn Martensen <bjoern.martensen@gmail.com>
2014-08-26 14:03:39 +0200

    fix user table migration

--- src/migrations/2014_08_26_072856_create_users_table.php
+++ src/migrations/2014_08_26_072856_create_users_table.php
@@ -16,9 +16,20 @@ class CreateUsersTable extends Migration {
             $table->engine = 'InnoDB';
             $table->increments('id');
 
-            $table->text('terms_of_use');
-            $table->text('privacy_policy');
-            $table->string('share_message', 255);
+            $table->string('fb_id')->unique();
+            $table->string('first_name')->nullable();;
+            $table->string('last_name')->nullable();;
+            $table->string('username', 255)->unique()->nullable();
+            $table->string('password', 255)->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('postal', 5)->nullable();
+            $table->string('city')->nullable();
+            $table->date('birthday')->nullable();
+            $table->boolean('privacy_agreement')->default(0)->nullable();
+            $table->boolean('contact_post')->default(0)->nullable();
+            $table->boolean('contact_email')->default(0)->nullable();
 
             $table->integer('tenant_id')->unsigned()->nullable();
             $table->foreign('tenant_id')
