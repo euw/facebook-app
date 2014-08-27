3d84c22c042d4ed7d9bd12a15c13430df7a1f4cb
Björn Martensen <bjoern.martensen@gmail.com>
2014-08-27 08:53:55 +0200

    fix migration

--- src/migrations/2014_08_27_072856_create_invitations_table.php
+++ src/migrations/2014_08_27_072856_create_invitations_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration {
+class CreateInvitationsTable extends Migration {
 
     /**
      * Run the migrations.
@@ -12,7 +12,7 @@ class CreateUsersTable extends Migration {
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('invitations', function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->increments('id');
 
@@ -44,7 +44,7 @@ class CreateUsersTable extends Migration {
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('invitations');
     }
 
 }
